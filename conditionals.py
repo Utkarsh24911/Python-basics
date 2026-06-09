7a9a403ae35be0d42da9ca1575b431e30c4f401e
vasuutkarsh5@gmail.com <vasuutkarsh5@gmail.com>
2026-06-09 12:11:41 +0530

    basics in python

---
+++ conditionals.py
@@ -0,0 +1,245 @@
+# if statement 
+
+# age=10
+
+# if(age>18):
+#     print("You are eligible")
+#     print("Thanks")
+
+# print("End of program")
+
+# if-else statement
+
+# age=int(input("Enter your age:"))
+
+# if(age>18):
+#     print("You can drive")
+
+# else:
+#     print("You cannot drive")
+
+
+# if-elif-else statement
+
+# age=int(input("Enter your age :"))
+
+# if(age>18):
+#     print("You can drive")
+
+# elif(age==18):
+#     print("Schedule an interview")
+
+# else:
+#     print('You cannot drive')
+
+
+
+# Match casae
+
+# a=int(input("Enter a number between 1 and 10 :"))
+
+# match a:
+#     case 1:
+#         print("You won a car toy")
+#     case 5:
+#         print("You won 100ruppee")
+#     case 7:
+#         print("You won an ice cream")
+#     case _:
+#         print("Oops! Better luck next time ")
+
+
+# for loops
+
+# for i in range(1,51):
+#     print(i)
+
+# for i in range(1,11):
+#     print("5 X" , i , "=" , 5*i)
+
+
+# while loop  
+
+# i = 20
+# while i >= 1:
+#     print (i)
+#     i = i-1
+
+# i = 1
+# while i < 8:
+#     print (i)
+#     i = i+1
+
+# i = 1
+# while True:
+#     print (i)
+#     i = i+1
+
+
+# break statement
+
+# for i in range(0,15):
+#     print(i)
+#     if i == 5:
+#         break
+
+# continue statement
+
+# for i in range (1,20):
+#     if i == 10:
+#         continue
+#     print (i)
+
+
+# for i in range (1,20):
+
+#     print (i)
+#     if i == 10:
+#         continue
+
+# pass statement
+
+# for i in range (5):
+    
+#     if i == 3:
+#         pass
+#     print(i)
+    
+
+
+# a=int(input("Enter a number:"))
+
+# if (a > 0):
+#     print("Its a positive number")
+
+# elif (a < 0):
+#     print("The number is a negative number ")
+
+# else:
+#     print("The number is zero")
+
+# age = int(input("Enter your age :"))
+
+# if (age >= 18):
+#     print(" You are eligible to vote ")
+
+# else:
+#     print(" You are not eligible to vote")
+
+
+
+# num = int(input("Enter a number :"))
+
+# if (num % 2 == 0):
+#     print("Its an even number ")
+
+# else:
+#     print("Its an odd number")
+
+# a=int(input("Enter a number between 1 and 7:\n"))
+
+# match a:
+#     case 1:
+#         print("Sunday")
+
+#     case 2:
+#         print("Monday")
+
+#     case 3:
+#         print("Tuesday")
+
+#     case 4:
+#         print("Wednesday")
+
+#     case 5:
+#         print("Thursday")
+
+#     case 6:
+#         print("Friday")
+
+    # case 7:
+    #     print("Saturday")
+
+
+# num1= int(input("Enter first number:\n"))
+# num2= int(input("Enter second number:\n"))
+
+# operation = input("Choose an operation:")
+
+# match operation:
+#     case "+":
+#         print(num1 + num2)
+
+#     case "-":
+#         print(num1 - num2)
+
+#     case "*":
+#         print(num1 * num2)
+
+#     case "/":
+#         print(int(num1 / num2))
+
+
+
+# for i in range (1,11):
+#     print(i)
+
+
+# n = int(input("Enter a number:"))
+
+# for i in range(1,11):
+#     print(n ,"X" , i , "=", n*i)
+
+# sum=0
+# for i in range (1,101):
+#     sum+= i
+
+# print(sum)
+ 
+
+# for i in range (1,5):
+#     print("*" * i)
+
+
+# sum = 0
+# i=1
+
+# while i<=10:
+#     print(i)
+#     sum+=i
+#     i += 1
+
+# print(sum)
+
+# password= "BJHJ123"
+# entered_password=input("Enter password:") 
+
+# while(entered_password != password):
+#     entered_password = input("Oops wrong password! Try Again :")
+
+# print("Successfully! Logged in")
+
+
+# num=44556
+
+# print(int(str(num)[::-1]))
+
+
+# for i in range(1,11):
+#     if i==7:
+#         break
+#     print(i)
+
+
+# for i in range(1,11):
+#     if i==5:
+#         continue
+#     print(i)
+
+
+# for i in range (1,6):
+#     if i == 3:
+#         pass
+#     print(i)
+
+
---
+++ string.py
@@ -0,0 +1,116 @@
+# name = "Vasu"
+# name = 'Vasu'
+# name = '''Vasu 
+# is 
+# a good boy'''
+# print (name)
+
+# INDEXING
+
+# name = "Utkarsh"
+
+# indexing from left to right = 0123456
+# indexing from right ot left = -1-2-3-4-5-6-7
+
+# print(name[0] , end="")
+# print(name[1] , end="")
+# print(name[2] , end="")
+# print(name[3] , end="")
+# print(name[4] , end="")
+# print(name[5] , end="")
+# print(name[6] , end="")
+
+# print(name[0])
+# print(name[1])
+# print(name[2])
+# print(name[3])
+# print(name[4])
+# print(name[5])
+# print(name[6])
+
+# print(name[-1])
+# print(name[-2])
+# print(name[-3])
+# print(name[-4])
+# print(name[-5])
+# print(name[-6])
+# print(name[-7])
+
+# SLICING
+
+# string slicing
+
+# name = "Utkarsh"
+
+# print(name[0:5]) goes from 0 to 5-1 i.e. from 0 to 4
+
+# print(name[3:-1]) goes from 3 to -1-1 i.e. from 3 to -2
+
+# step slicing
+
+# name="Vasu1234567"
+
+# print(name[0:10:n]) skip n-1 characters
+# print(name[0:10:1]) skip 1-1 ie 0 characters
+# print(name[0:10:3]) skip 3-1 ie 2 characters
+
+
+# print(name[:4]) Replace the first empty place with 0 and it will become [0:4]
+
+# print(name[1:]) Replace the second empty place with lenght of variable and it will becone name[1:11]
+
+# name="Utkarsh" #strings are immutabel
+
+# # name[0]= "R" you cannot change this
+
+# a=len(name)
+# print(a)
+
+# t="hello world" #strings are immutabel
+
+# name[0]= "R" you cannot change this
+
+# a=len(t)
+# print(a)
+
+# print(t.upper())
+
+# print(t.lower())
+
+# print(t.capitalize())/
+
+# print(t.title())
+
+# text = "  hello world  "
+# print(text.strip()) 
+# print(text.lstrip()) 
+# print(text.rstrip())
+
+# text = "Python is fun is also fun to much fun and fun"
+# print(text.find("is"))  
+# print(text.replace("fun", "awesome"))
+
+# text = "apple,banana,orange"
+# print(text.split(","))
+# print(",".join(['apple', 'banana', 'orange']))
+
+
+# text = "Python123"
+# print(text.isalpha())  
+# print(text.isalnum()) 
+# print(text.isspace())  
+
+# fstring 
+
+# template = "Dear {}, you are awesome . Take this {}."
+
+# a="Vasu"
+# a1="icecream"
+
+# # s1=template.format(a,a1)
+# # print(s1)
+
+# print(f"{a} you are awesome . Take this {a1}.")
+
+
+
